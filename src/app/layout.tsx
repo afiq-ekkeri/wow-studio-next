3ebcec7c30693ebc7c079347317c22035500e019
afiq-ekkeri <afiqeky@gmail.com>
2024-09-24 03:10:23 +0530

    robots.txt update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './globals.css'
 import GoogleAnalytics from '@/app/providers/GoogleAnalytics';
 import type { Metadata } from 'next'
-import Head from 'next/head'
 
 import localFont from 'next/font/local'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 const georgia = localFont({ src: '../fonts/Georgia.woff2' })
 const blatant = localFont({ src: '../fonts/BlatantRegular.woff2' })
@@ -15,7 +11,8 @@ const blatant = localFont({ src: '../fonts/BlatantRegular.woff2' })
 export const metadata: Metadata = {
   title: 'Digital Branding Creative Agency in Dubai',
   description: 'Wow Studio is a creative brand agency in Dubai. We help build brands that leaves a lasting impression, brands with purpose, which impact the lives of millions.',
-  keywords: "creative studio, brand strategy, brand identity, design strategy, product design, marketing strategy, graphic design, brochure design, wow studio"
+  keywords: "creative studio, brand strategy, brand identity, design strategy, product design, marketing strategy, graphic design, brochure design, wow studio",
+  robots: 'noindex,nofollow'
 }
 
 export default function RootLayout({
@@ -25,9 +22,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="robots" content="noindex,nofollow" />
-      </Head>
       <GoogleAnalytics />
       <body className={`${georgia.className} ${blatant.className}`}>{children}</body>
     </html>
